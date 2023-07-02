fbf6e0388b7d0a3fe020490a13cd946918ce6bd3
Hugo19S <adyseta1998@outlook.com>
2023-07-02 19:04:10 +0100

    Login a funcionar e todo o projeto está pronto

--- app/src/main/java/com/example/fragments/SigIn.java
+++ app/src/main/java/com/example/fragments/SigIn.java
@@ -1,13 +1,8 @@
 package com.example.fragments;
 
-import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Intent;
-import android.content.res.AssetManager;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,21 +10,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import org.json.JSONArray;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class SigIn extends Fragment {
 
@@ -83,48 +73,66 @@ public class SigIn extends Fragment {
     }
 
     public void readJson(View view) {
-        try {
 
-            // Cria um objeto de arquivo com o caminho e nome do arquivo
-            File file = new File(requireContext().getFilesDir(), "arquivo.json");
-
-            // Lê o conteúdo do arquivo
-            StringBuilder stringBuilder = new StringBuilder();
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                stringBuilder.append(line);
-            }
-            reader.close();
-
-            String jsonString = stringBuilder.toString();
-
-            // Processar a string JSON conforme necessário
-            JSONObject jsonObject = new JSONObject(jsonString);
-            this.userName = jsonObject.getString("userName");
-            this.email = jsonObject.getString("email");
-            this.password = jsonObject.getString("password");
-
-        } catch (IOException | JSONException e) {
-            e.printStackTrace();
-        }
-
-        validateCredential(view);
-    }
-
-    public void validateCredential(View view) {
         //recebe o valor introduzido no campo do email e password na tela de login
         EditText email_field = view.findViewById(R.id.editTextEmai);
         EditText pass_field = view.findViewById(R.id.editTextPassword);
         String email_in = email_field.getText().toString();
         String pass_in = pass_field.getText().toString();
+        try {
 
-        if (email_in.equals("") || pass_in.equals("")) {
-            Toast.makeText(requireContext(), "Existem campos vazios!", Toast.LENGTH_SHORT).show();
-        } else if (this.email.equals(email_in) && this.password.equals(pass_in)) {
+            if (!email_in.equals("") && !pass_in.equals("")) {
+                // Cria um objeto de arquivo com o caminho e nome do arquivo
+                File file = new File(requireContext().getFilesDir(), "arquivo.json");
+
+                // Lê o conteúdo do arquivo
+                StringBuilder stringBuilder = new StringBuilder();
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                reader.close();
+
+                String jsonString = stringBuilder.toString();
+
+                // Processar a string JSON conforme necessário
+                JSONObject jsonObject = new JSONObject(jsonString);
+                this.userName = jsonObject.getString("userName");
+                this.email = jsonObject.getString("email");
+                this.password = jsonObject.getString("password");
+
+                validateCredential(view, email_in, pass_in);
+            } else {
+                Toast.makeText(requireContext(), "Existem campos vazios!", Toast.LENGTH_SHORT).show();
+            }
+
+
+        } catch (IOException | JSONException e) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(requireContext());
+            dialog.setTitle("Login");
+            dialog.setMessage("Utilizador não encontrado!");
+            dialog.setCancelable(false);
+            dialog.setPositiveButton("Ok", (dialogInterface, i) -> {
+
+            });
+            dialog.create().show();
+        }
+    }
+
+    public void validateCredential(View view, String email_in, String pass_in) {
+
+        if (this.email.equals(email_in) && this.password.equals(pass_in)) {
             Toast.makeText(requireContext(), "Login feito com sucesso!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(requireContext(), MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
         } else {
             Toast.makeText(requireContext(), "Credenciais errados!", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void dialogBox() {
+
     }
 }
